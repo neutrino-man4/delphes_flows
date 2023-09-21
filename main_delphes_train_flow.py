ad91a93cfc44df0b69c3d8dd1dca55029418fce8
Aritra <aritrabal2@gmail.com>
2023-09-21 15:13:48 +0200

    Set up initial master training script

--- main_delphes_train_flow.py
+++ main_delphes_train_flow.py
@@ -1,5 +1,10 @@
+import delphes_flows.flows.flow_catalogue as fc
+import os
 import numpy as np
-import tensorflow_probability as tfp
 
-tfb=tfp.bijectors()
-
+import pofah.path_constants.sample_dict_file_parts_input as sdi
+import pofah.util.experiment as expe
+import pofah.util.sample_factory as safa
+import vande.util.data_generator as dage
+import sarewt.data_reader as dare
+import pofah.phase_space.cut_constants as cuts
