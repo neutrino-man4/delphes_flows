f57c575c348d6a7f1add43b9587482a397eda7f5
Aritra <aritrabal2@gmail.com>
2023-09-21 15:18:26 +0200

    Updates

---
+++ paths/data_path.py
@@ -0,0 +1,7 @@
+path_dict={
+    'base_dir':'/work/abal/CASE/delphes_flows/saved_models/run_XYZ'
+}
+
+sample_dict={
+    'qcddataSB_CMS_Reco':'/storage/9/abal/CASE/VAE_results/events/run_50005/qcd_data_sideband_RECO/'
+}
---
+++ paths/model_path.py
@@ -0,0 +1,3 @@
+path_dict={
+    'base_dir':'/work/abal/CASE/delphes_flows/saved_models/run_XYZ'
+}
