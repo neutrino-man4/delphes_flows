042a026ac9ebac4acde19b11339878a763afd4e9
Aritra <aritrabal2@gmail.com>
2023-09-21 15:12:18 +0200

    Added bool flag to forward step function to deal with both training and validation steps

--- flows/flow_base.py
+++ flows/flow_base.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 from typing import Sequence
+import os
 tfd=tfp.distributions
 tfb=tfp.bijectors
-from scipy.stats import wasserstein_distance
 
 class NormalizingFlow(tf.keras.models.Model):
 
@@ -14,15 +14,16 @@ class NormalizingFlow(tf.keras.models.Model):
         self.flow=tfd.TransformedDistribution(distribution=self.base_distribution,bijector=bijector)
         super(NormalizingFlow,self).__init__(**kwargs)
 
-    def __call__(self,*inputs):
-        return self.flow.bijector.forward(*inputs)
+    def __call__(self,input):
+        return self.flow.bijector.forward(input)
     
     @tf.function
-    def train_step(self,X,optimizer):
+    def forward_step(self,X,optimizer,training=True):
         with tf.GradientTape() as tape:
             loss = -tf.reduce_mean(self.flow.log_prob(X, training=True))
-            gradients = tape.gradient(loss, self.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+            if training:
+                gradients = tape.gradient(loss, self.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         return loss
     
 class JetFlow(tf.keras.models.Model):
@@ -35,22 +36,25 @@ class JetFlow(tf.keras.models.Model):
         self.flow=tfd.TransformedDistribution(distribution=self.base_distribution,bijector=bijector)
         super(JetFlow,self).__init__(**kwargs)
 
-    def __call__(self,*inputs):
-        return self.flow.bijector.forward(*inputs)
-  
+    def __call__(self,input):
+        return self.flow.bijector.forward(input)
 
     @tf.function
-    def train_step(self, X,Y,optimizer):
+    def forward_step(self, X_batch,Y_batch,optimizer,loss_fn,training=True):
         with tf.GradientTape() as tape:
-            # Compute the Wasserstein distance
-            wd=[]
-            reco_flow=self.flow.sample(X)
-            for i in range(reco_flow.shape[-1]):
-                w_distance.append(wasserstein_distance(reco_flow[:,i],Y[:,i]))
-            w_distance=tf.reduce_mean(tf.stack(w_distance))
-            loss = tf.reduce_mean(w_distance)
-            gradients = tape.gradient(loss, self.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+            tape.watch(self.flow.trainable_variables)
+            orig_flow=self.flow.sample(X_batch) # Shape: batch size x 300
+            loss=loss_fn(tf.reshape(orig_flow,[-1,100,3]),tf.reshape(Y_batch,[-1,100,3])) # Reshape to batch size x 100 x 3 to be compatible with loss
+            if training:
+                gradients = tape.gradient(loss, self.flow.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, self.flow.trainable_variables))
         return loss
+    
+    def save_model(self,path,fname='jetMAF.tf'):
+        model_name=os.path.join(path,fname)
+        self.save(model_name,save_format='tf')
+    
+    def load_model(self,path,custom_objects={},fname='jetMAF.tf'):
+        model_name=os.path.join(path,fname)
+        return tf.keras.saving.load_model(model_name,custom_objects=custom_objects)
 
-      
