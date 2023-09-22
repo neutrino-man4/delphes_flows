ca659c1dd45f95e7c1aa75e96908a992723b73cb
Aritra <aritrabal2@gmail.com>
2023-09-22 12:22:57 +0200

    Updated train() method to accept single tf dataset instead of separate x_train,y_train

--- utils/trainer.py
+++ utils/trainer.py
@@ -18,27 +18,31 @@ class Trainer():
         self.train_stop = Stopper(optimizer, min_delta, patience, max_lr_decay,lr_decay_factor)
         self.best_loss_so_far = None    
     
-    def training_epoch(self,model,X_train,Y_train,loss_fn):
+    def training_epoch(self,model,train_ds,loss_fn):
         train_loss=0
-        for step,(X_batch,Y_batch) in enumerate(zip(X_train,Y_train)):
+        for step,(orig_batch,reco_batch) in enumerate(train_ds):
             loss=model.forward_step(X_batch,Y_batch,self.optimizer,loss_fn,training=True)
             train_loss+=loss
         return train_loss/(step+1)
     
     def valid_epoch(self,model,X_valid,Y_valid,loss_fn):
         valid_loss=0
-        for step,(X_batch,Y_batch) in enumerate(zip(X_valid,Y_valid)):
+        for step,X_batch,Y_batch in enumerate(zip(X_valid,Y_valid)):
             loss=model.forward_step(X_batch,Y_batch,self.optimizer,loss_fn,training=False)
             valid_loss+=loss
         return valid_loss/(step+1)
     
     
-    def train(self,model,X_train,Y_train,X_valid,Y_valid,loss_fn,epochs,training=True):
+    def train(self,model,train_ds,valid_ds,loss_fn,epochs,training=True):
+        '''
+        train_ds: should contain batches of both orig and reco jets in that order
+        valid_ds: should contain two datasets: orig and reco, in that order
+        '''
         losses_train=[]
         losses_valid=[]
         for epoch in range(epochs):
             print(f'Epoch: {epoch}')
-            train_loss=self.training_epoch(model,X_train,Y_train,loss_fn)
+            train_loss=self.training_epoch(model,train_ds,loss_fn)
             valid_loss=self.valid_epoch(model,X_valid,Y_valid,loss_fn)
             losses_train.append(train_loss)
             losses_valid.append(valid_loss)
