ebba081395a9464f9e16bb575a6de0532432fd69
Aritra <aritrabal2@gmail.com>
2023-09-26 14:33:54 +0200

    Moved class initialization to flow_catalogue.py and added methods to calculate NLL and invert flow

--- flows/flow_base.py
+++ flows/flow_base.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from typing import Sequence
 import os
+
 tfd=tfp.distributions
 tfb=tfp.bijectors
 
@@ -12,7 +13,7 @@ class NormalizingFlow(tf.keras.models.Model):
         self.flow_layers=flow_layers
         bijector=tfb.Chain(list(reversed(self.flow_layers)))
         self.flow=tfd.TransformedDistribution(distribution=self.base_distribution,bijector=bijector)
-        super(NormalizingFlow,self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
     def __call__(self,input):
         return self.flow.bijector.forward(input)
@@ -32,33 +33,33 @@ class JetFlow(tf.keras.models.Model):
     Base distribution: kinematics of reco jet
     target distribution: kinematics of orig. jet
     '''
-    def __init__(self,base_distribution,flow_layers:Sequence,**kwargs) -> None:
-        self.base_distribution=base_distribution
-        self.flow_layers=flow_layers
-        bijector=tfb.Chain(list(reversed(self.flow_layers)))
-        self.flow=tfd.TransformedDistribution(distribution=self.base_distribution,bijector=bijector)
-        super(JetFlow,self).__init__(**kwargs)
-
-    def __call__(self,input):
-        return self.flow.bijector.forward(input)
+    def __init__(self,**kwargs) -> None:
+        super().__init__(**kwargs)
+        self.flow=None
+        
+    def __call__(self,*input):
+        return self.flow.bijector.forward(*input)
 
     @tf.function
-    def forward_step(self, X_batch,Y_batch,optimizer,loss_fn,training=True):
+    def forward_step(self, X_batch,optimizer,training=True):
         '''
         For training,
-        X_batch: reco jets
-        Y_batch: orig jets
-        orig_flow: "original" jet as reconstructed by the flow
-        loss function - threeD loss (as used by VAE) computed between orig_flow and Y_batch (actual original jets)
+        X_batch: reco/orig jets
+        Ignore: This is wrong implementation
         '''
         with tf.GradientTape() as tape:
-            tape.watch(self.flow.trainable_variables)
-            orig_flow=self.flow.sample(X_batch) # Shape: batch size x 100 x 3
-            loss=loss_fn(Y_batch,orig_flow) # Shape: batch size x 100 x 3 to be compatible with threeD loss
-            if training:
-                gradients = tape.gradient(loss, self.flow.trainable_variables)
-                optimizer.apply_gradients(zip(gradients, self.flow.trainable_variables))
+            tape.watch(self.trainable_variables)
+            loss=-tf.reduce_mean(self.flow.log_prob(X_batch,training=training))
+        if training:
+            gradients = tape.gradient(loss, self.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         return loss
+    
+    def nll_loss(self,X_batch,training=False):
+        return tf.math.abs(-tf.reduce_mean(self.flow.log_prob(X_batch,training=training)))
+    
+    def invert(self,X_batch):
+        return self.flow.bijector.inverse(X_batch)
     
     def save_model(self,path,fname='jetMAF.tf'):
         model_name=os.path.join(path,fname)
